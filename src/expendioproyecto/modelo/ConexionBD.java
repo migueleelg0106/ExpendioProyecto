37b070ef6a7ed1fa54fb10234ff623dfa42c9587
UrielCendon <urielcd2013@gmail.com>
2025-05-25 03:23:15 -0600

    conexion en config.properties

--- src/expendioproyecto/modelo/ConexionBD.java
+++ src/expendioproyecto/modelo/ConexionBD.java
@@ -4,36 +4,67 @@
  */
 package expendioproyecto.modelo;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
- *
- * @author uriel
- */
 public class ConexionBD {
-    private static final String IP = "localhost";
-    private static final String PUERTO = "3306";
-    private static final String NOMBRE_BD = "expendio";
-    private static final String USUARIO = "root";
-    private static final String PASSWORD = "UrieLCD42024";
+
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-            
-    public static Connection abrirConexion(){
+
+    public static Connection abrirConexion() {
         Connection conexionBD = null;
-        String urlConexion = String.format("jdbc:mysql://%s:%s/%s?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC", IP, PUERTO, NOMBRE_BD);
-        
+        String[] credentials = getCredentials();
+
         try {
-           Class.forName(DRIVER);
-           conexionBD = DriverManager.getConnection(urlConexion, USUARIO, PASSWORD);
-        } catch(ClassNotFoundException e){
-            e.printStackTrace();
-            System.err.println("Error Clase no encontrada.");
-        } catch (SQLException s){
-            s.printStackTrace();
-            System.err.println("Error en la conexion: "+ s.getMessage());
+            Class.forName(DRIVER);
+            conexionBD = DriverManager.getConnection(credentials[0], credentials[1], credentials[2]);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
         }
+
         return conexionBD;
-    }        
+    }
+
+    private static String[] getCredentials() {
+        String[] credentials = new String[3];
+
+        try (InputStream input = ConexionBD.class.getClassLoader()
+                .getResourceAsStream("expendioproyecto/config.properties")) {
+
+            Properties properties = new Properties();
+            properties.load(input);
+
+            credentials[0] = properties.getProperty("DB_URL");
+            credentials[1] = properties.getProperty("DB_USER");
+            credentials[2] = properties.getProperty("DB_PASSWORD");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return credentials;
+    }
+
+    public static String getAESKEY() {
+        String AES_KEY = "";
+
+        try (InputStream input = ConexionBD.class.getClassLoader()
+                .getResourceAsStream("expendioproyecto/config.properties")) {
+
+            Properties properties = new Properties();
+            properties.load(input);
+            AES_KEY = properties.getProperty("AES_KEY");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return AES_KEY;
+    }
 }
